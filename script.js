09f43a6d454ca540712fe8bc72c9aff971bd9852
José Jonas <87939629+Jo-onas@users.noreply.github.com>
2022-01-15 19:15:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,21 @@
         handleSlide()
     }, 4000);
 
+
+        const slider = document.querySelector(".slider2")
+
+        let widthSlide = 0
+
+        function passarSlide() {
+            if(widthSlide == -300) {
+            widthSlide = 0
+            }
+            slider.style.marginLeft = `${widthSlide}vw`
+        }
+
+        setInterval(() => {
+            widthSlide += -100
+            passarSlide()
+        }, 4000);
+
+
